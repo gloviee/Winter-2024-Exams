f7e11527460a62d7a9352f0aa4b9b5836ddd5b74
exluse <114081542+gloviee@users.noreply.github.com>
2024-01-14 22:01:14 +0200

    5-invert.js

--- Tasks/invert.js
+++ Tasks/invert.js
@@ -9,7 +9,7 @@
 
 const invertArray = (array) => {
   const reversedArray = [];
-  for (let i = array.length; i >= 0; i--) {
+  for (let i = array.length - 1; i >= 0; i--) {
     reversedArray.push(array[i]);
   }
   return reversedArray;
