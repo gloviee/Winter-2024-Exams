ef2a317cd2a9a25324fd00ecb0a46fec601b4d01
exluse <114081542+gloviee@users.noreply.github.com>
2024-01-14 21:03:14 +0200

    7-zip.js

--- Tasks/zip.js
+++ Tasks/zip.js
@@ -1,8 +1,7 @@
 // Zip two arrays, [1, 2] and [3, 4] -> [[1, 3], [2, 4]]
 
-// Step 6
-// add variable type 
-// add list elements check
+// Step 7
+// understood that i can't put a into for loop with of operator, so rewrite the loop and make code more readable
 
 'use strict';
 
@@ -10,9 +9,8 @@
 const zip = (array1, array2) => {
   let i = 0;
   const zippedArray = []
-  for (const element of Math.min(array1.length, array2.length)) {
-    let CELL = [array1[i++], element];
-    let changedElement = CELL
+  for (i; i < Math.min(array1.length, array2.length); i++) {
+    let changedElement = [array1[i], array2[i]]
     zippedArray.push(changedElement)
   }
   
