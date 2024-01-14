7a2593499c388051b3a3cccf60d6f8ae6acc7557
exluse <114081542+gloviee@users.noreply.github.com>
2024-01-14 18:14:31 +0200

    2-intersection.js

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,24 +1,31 @@
 // Find an intersection of two dictionaries
 
-// Step 1
-// Add 'use strict';
-// Add variables types
-// Remove empty blocks
-// Fix format
-// Make better Style
+// Step 2
+// Add conditions for avoid incorrect format
+// Add for loop
+// Add object1 size argument 
+// Add array for save intersections values from all dicctionaries
+// remove unneedable blocks
 
 'use strict';
 
-const intersection = (object1, object2) => {
-  const firstKeys = Object.keys(object1);
-  for (attributeName of firstKeys) {
-    if (object1[attributeName] === object2[attributeName]) {
-      object2[attributeName] = object1[attributeName];
-    } 
-    else {
-      delete object1[attributeName];
+const intersectionArray = [];
+
+const intersection = (object1, object2, size = object1.length) => {
+  for (let i = 0; i < size; i++) {
+    if(typeof object1[i] === 'object') {
+      const firstKeys = Object.keys(object1);
+      for (attributeName of firstKeys) {
+        if (object1[attributeName] === object2[attributeName]) {
+        object2[attributeName] = object1[attributeName];
+        } 
+        else {
+          delete object1[attributeName];
+        }
+      }
     }
-  return object1;
+  }
+  return intersectionArray;
 };
 
 module.exports = intersection;
