49f300a915d0a25d46f50edc7c007d5bc6c3a6f3
exluse <114081542+gloviee@users.noreply.github.com>
2024-01-14 20:16:39 +0200

    6-zip.js

--- Tasks/zip.js
+++ Tasks/zip.js
@@ -1,6 +1,6 @@
 // Zip two arrays, [1, 2] and [3, 4] -> [[1, 3], [2, 4]]
 
-// Step 5
+// Step 6
 // add variable type 
 
 'use strict';
@@ -11,7 +11,7 @@ const zip = (array1, array2) => {
   let i = 0;
   let j = 0;
   for (const element of array2) {
-    CELL = [array1[i++], element];
+    let CELL = [array1[i++], element];
     let changedElement = CELL
     zippedArray.push(changedElement)
   }
