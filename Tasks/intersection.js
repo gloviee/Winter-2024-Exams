c2a47b20be48702a35e3bac55ddd0dff289d09cf
exluse <114081542+gloviee@users.noreply.github.com>
2024-01-14 20:10:56 +0200

    remaked-intersection.js

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -12,14 +12,13 @@
 // don't change incoming data 
 // remove else block 
 
-
 'use strict';
 
 const intersectionArray = {}
 
 const intersection = (object1, object2) => {
   const firstKeys = Object.keys(object1);
-  for (attributeName of firstKeys) {
+  for (const attributeName of firstKeys) {
     if (object1[attributeName] === object2[attributeName]) {
       object2[attributeName] = object1[attributeName];      
       intersectionArray[attributeName] = object1[attributeName];
