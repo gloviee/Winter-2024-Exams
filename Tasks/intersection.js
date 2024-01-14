f9d7fe726a0a49f80e3284b40498bf0757f7dad3
exluse <114081542+gloviee@users.noreply.github.com>
2024-01-14 20:47:23 +0200

    fixed-intersection.js

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -10,22 +10,18 @@
 // add variables type
 // Fix format 
 // don't change incoming data 
+// remove else block
 
 'use strict';
 
-const intersectionArray = {}
-
 const intersection = (object1, object2) => {
   const firstKeys = Object.keys(object1);
-  
+  const intersectionArray = {}
   for (const attributeName of firstKeys) {
     if (object1[attributeName] === object2[attributeName]) {
       object2[attributeName] = object1[attributeName];      
       intersectionArray[attributeName] = object1[attributeName];
     } 
-    else {
-      delete object1[attributeName];
-    }
   }
   return intersectionArray;
 };
