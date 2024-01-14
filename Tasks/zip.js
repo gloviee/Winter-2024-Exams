4fc9dc4de42575cff06b820480e553c202d4739d
exluse <114081542+gloviee@users.noreply.github.com>
2024-01-14 16:46:09 +0200

    4-zip.js

--- Tasks/zip.js
+++ Tasks/zip.js
@@ -1,19 +1,23 @@
 // Zip two arrays, [1, 2] and [3, 4] -> [[1, 3], [2, 4]]
 
-// Step 3
-//rename variables 
+// Step 4
+//don't change incoming data
+//remove brackets, change it to a more comprehensible code
 
 'use strict';
+
+const zippedArray = []
 
 const zip = (array1, array2) => {
   let i = 0;
   let j = 0;
   for (element of array2) {
     CELL = [array1[i++], element];
-    (() => (array2[j++] = CELL))();
+    let changedElement = CELL
+    zippedArray.push(changedElement)
   }
   
-  return array2;
+  return zippedArray;
 };
 
 module.exports = zip;
