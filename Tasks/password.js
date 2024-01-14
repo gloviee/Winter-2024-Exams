3e2cf8185d385d14fd1a2461a3105d5d0c2a67a1
exluse <114081542+gloviee@users.noreply.github.com>
2024-01-14 19:39:00 +0200

    1-password.js

--- Tasks/password.js
+++ Tasks/password.js
@@ -1,10 +1,18 @@
 // Generate random password
 
-let GeneratePassword = (alphabet, length) => {
+// Step 1
+// add 'use strict';
+// prefer const instead of let
+// add variables type
+
+
+'use strict';
+
+const GeneratePassword = (alphabet, length) => {
   const MAX = alphabet.length;
-  key = '';
+  let key = '';
   for (let i = 0; i < length; i++) {
-    Index = Math.floor(Math.random() * MAX);
+    const index = Math.floor(Math.random() * MAX);
     key = key + alphabet[Index];
   }
   return key;
