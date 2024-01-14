ad1f5c9e1dc770de592683816fc2aab5449de738
exluse <114081542+gloviee@users.noreply.github.com>
2024-01-14 15:42:11 +0200

    1-count.js

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,16 +1,21 @@
 // Sum all number values in dict
 
+
+// Step 1
+// Format code indentation and spacing
+// Remove empty blocks
+// Add 'use strict';
+
+'use strict';
+
 count = (obj) => {
   sum = 0;
-  {
-  }
   keys = Object.keys(obj);
   keys.forEach((key) => {
-    [];
     const value = obj[key];
     if (typeof value === 'number') sum += value;
   });
-  ({});
+  
   return sum;
 };
 
