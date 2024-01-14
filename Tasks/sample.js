5f428c9b4d934e4d9d2f8eff5cb13766276480f1
exluse <114081542+gloviee@users.noreply.github.com>
2024-01-14 20:08:28 +0200

    fixed-sample.js

--- Tasks/sample.js
+++ Tasks/sample.js
@@ -1,13 +1,12 @@
 // Get one random element from an array
 
-// Step 1
-// add 'use strict';
-// rename variables 
+// Step 2
+// fix my imprint
 
 'use strict';
 
 const sample = (array) => {
-  randomValue = arr[Math.floor(Math.random() * arr.length)];
+  randomValue = array[Math.floor(Math.random() * arr.length)];
   return randomValue;
 };
 
