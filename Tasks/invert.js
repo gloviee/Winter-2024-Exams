0ba7db85e4041c003ceb0ceb60da1cd617dada64
exluse <114081542+gloviee@users.noreply.github.com>
2024-01-14 20:06:27 +0200

    Update invert.js

--- Tasks/invert.js
+++ Tasks/invert.js
@@ -1,17 +1,16 @@
 // Reverse an array, you can't use .reverse()
 
-// Step 3
+// Step 4
 // Fix my imprint
-// rename variable
-// remove argument 
+
 
 
 'use strict';
 
 const invertArray = (array) => {
-  elements = Object.keys(A);
+  elements = Object.keys(array);
   for (let i = 0; i < elements.length; i++) {
-    elements[i] = A.pop();
+    elements[i] = array.pop();
   }
   return elements;
 };
