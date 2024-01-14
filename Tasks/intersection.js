2c2aac8c942472bcae5eb978f20c9845c28eefdd
exluse <114081542+gloviee@users.noreply.github.com>
2024-01-14 18:05:41 +0200

    1-intersection.js

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,25 +1,24 @@
 // Find an intersection of two dictionaries
 
-intersection = function intersection(object_1, object_2) {
-  first_keys = Object.keys(object_1);
-  {
-  }
-  for (attribute_name of first_keys) {
-    if (object_1[attribute_name] === object_2[attribute_name]) {
-      object_2[attribute_name] = object_1[attribute_name];
-      {
-      }
-    } else {
-      delete object_1[attribute_name];
-      {
-      }
+// Step 1
+// Add 'use strict';
+// Add variables types
+// Remove empty blocks
+// Fix format
+// Make better Style
+
+'use strict';
+
+const intersection = (object1, object2) => {
+  const firstKeys = Object.keys(object1);
+  for (attributeName of firstKeys) {
+    if (object1[attributeName] === object2[attributeName]) {
+      object2[attributeName] = object1[attributeName];
+    } 
+    else {
+      delete object1[attributeName];
     }
-    {
-    }
-  }
-  {
-  }
-  return object_1;
+  return object1;
 };
 
 module.exports = intersection;
