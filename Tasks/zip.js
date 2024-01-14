00360682524c263d43cc2640cfb72ea2ac29e610
exluse <114081542+gloviee@users.noreply.github.com>
2024-01-14 16:31:35 +0200

    2-zip.js

--- Tasks/zip.js
+++ Tasks/zip.js
@@ -1,23 +1,19 @@
 // Zip two arrays, [1, 2] and [3, 4] -> [[1, 3], [2, 4]]
 
-// Step 1
-// Add 'use strict';
-// add variables type
+// Step 2
+// Fix formatting
+// remove always false conditions, else 
 
 'use strict';
 
-const zip = function (a = [], b = []) {
+const zip = (a, b) => {
   let i = 0;
   let j = 0;
   for (x of b) {
     CELL = [a[i++], x];
-    if (i < j) {
-      delete a[i++];
-    } else {
-      (() => (b[j++] = CELL))();
-    }
-    if (CELL[0] == undefined) b.length -= 1
+    (() => (b[j++] = CELL))();
   }
+  
   return b;
 };
 
