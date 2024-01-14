6fa8f5cb4ab3fda2e1271dc6ac2ccaf1a7f7016c
exluse <114081542+gloviee@users.noreply.github.com>
2024-01-14 18:46:25 +0200

    remaked-intersection.js

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,29 +1,29 @@
 // Find an intersection of two dictionaries
 
-// Step 2
-// Add conditions for avoid incorrect format
-// Add for loop
-// Add object1 size argument 
-// Add array for save intersections values from all dicctionaries
-// remove unneedable blocks
+// Noticed that intersection.test.js included 3 objects, where 3rd object was output
+// I thought we have a list of dictionaries and a dictionary one. 
+// Here i will remake it 
+
+// All steps
+// remove empty blocks
+// improve style
+// add variables type
+// Fix format 
+// don't change incoming data 
+// remove else block 
+
 
 'use strict';
 
-const intersectionArray = [];
+const intersectionArray = {}
 
-const intersection = (object1, object2, size = object1.length) => {
-  for (let i = 0; i < size; i++) {
-    if(typeof object1[i] === 'object') {
-      const firstKeys = Object.keys(object1);
-      for (attributeName of firstKeys) {
-        if (object1[attributeName] === object2[attributeName]) {
-        object2[attributeName] = object1[attributeName];
-        } 
-        else {
-          delete object1[attributeName];
-        }
-      }
-    }
+const intersection = (object1, object2) => {
+  const firstKeys = Object.keys(object1);
+  for (attributeName of firstKeys) {
+    if (object1[attributeName] === object2[attributeName]) {
+      object2[attributeName] = object1[attributeName];      
+      intersectionArray[attributeName] = object1[attributeName];
+    } 
   }
   return intersectionArray;
 };
